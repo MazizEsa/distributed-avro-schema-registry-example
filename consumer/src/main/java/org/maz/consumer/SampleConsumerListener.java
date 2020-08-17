650ccf975f17f618d3ab78f0ba1be245855a3d18
Maziz <maziz.esa@gmail.com>
2020-08-17 21:51:20 +0800

    Completed the consumer

---
+++ consumer/src/main/java/org/maz/consumer/SampleConsumerListener.java
@@ -0,0 +1,23 @@
+package org.maz.consumer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.maz.schema.FlatSampleData;
+import org.maz.schema.SplittedSampleData;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class SampleConsumerListener {
+    @KafkaListener(topics = {"flat-schema-topic"}, groupId = "flat")
+    public void receiveFlatSampleMessage(final ConsumerRecord<String, FlatSampleData> consumerRecord) {
+        log.info("Flat Data Received. The data is: {}", consumerRecord.value().getFlatEnumSample());
+    }
+
+    @KafkaListener(topics = {"splitted-schema-topic"}, groupId = "splitted")
+    public void receiveSplittedMessage(final SplittedSampleData splittedSampleData) {
+        log.info("Splitted Data Received. The data is: {}", splittedSampleData);
+    }
+}
--- producer/src/main/java/org/maz/producer/ProducerController.java
+++ producer/src/main/java/org/maz/producer/ProducerController.java
@@ -3,6 +3,8 @@ package org.maz.producer;
 import lombok.RequiredArgsConstructor;
 import org.maz.schema.FlatEnumerationSample;
 import org.maz.schema.FlatSampleData;
+import org.maz.schema.SplittedEnumerationSample;
+import org.maz.schema.SplittedSampleData;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +18,23 @@ public class ProducerController {
     private final KafkaMessageService kafkaMessageService;
 
     @PostMapping("/trigger-flat")
-    public ResponseEntity<String> triggerSendFlatFile() {
+    public ResponseEntity<String> triggerSendFlatMessage() {
         final FlatSampleData flatSampleData = FlatSampleData.newBuilder()
                 .setFlatSampleData("This is an example")
                 .setFlatEnumSample(FlatEnumerationSample.SAMPLE_1)
                 .build();
         kafkaMessageService.sendFlatSampleData(flatSampleData);
+
+        return ResponseEntity.ok("Ok");
+    }
+
+    @PostMapping("/trigger-splitted")
+    public ResponseEntity<String> triggerSendSplittedMessage() {
+        final SplittedSampleData splittedSampleData = SplittedSampleData.newBuilder()
+                .setSplittedSampleData("This is a splitted data, schema reference")
+                .setSplittedEnumSample(SplittedEnumerationSample.SAMPLE_2)
+                .build();
+        kafkaMessageService.sendSplittedSampleData(splittedSampleData);
 
         return ResponseEntity.ok("Ok");
     }
